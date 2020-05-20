08430e8f90bf71aa1519d24de753d87abb273ade
Sai Krishna Das <saikrishnadas666@gmail.com>
2020-05-20 13:31:11 +0530

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:time_tracker/app/landing_page.dart';
+import 'package:time_tracker/services/auth.dart';
+
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Time Tracker',
+      theme: ThemeData(
+        primarySwatch: Colors.indigo,
+      ),
+      home: LandingPage(
+        auth: Auth(),
+      ),
+    );
+  }
+}
